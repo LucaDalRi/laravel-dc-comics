a648e3fa0f8cda28cf1f35cb5fe6b35c2d8b0f07
LucaDalRi <euro2xd@gmail.com>
2023-03-15 22:25:31 +0100

    Creato modale di conferma eliminazione fumetto

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -7,11 +7,30 @@
                 <h1 class="text-center">
                     {{ $comic->title }}
                 </h1>
-                <a href="{{ route('comics.edit', $comic->id) }}" class="btn btn-warning">Modifica fumetto</a>
+                <a href="{{ route('comics.edit', $comic->id) }}" class="btn btn-warning">Modifica</a>
                 <form action="{{ route('comics.destroy', $comic->id) }}" method="POST">
                 @csrf
                 @method('DELETE')
-                <button class="btn btn-danger" type="submit">Elimina</button>
+                <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    Elimina
+                </button>
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h1 class="modal-title fs-5" id="exampleModalLabel">ARE YOU SURE ABOUT THAT?</h1>
+                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                          Sei sicuro di voler eliminare il fumetto?
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Chiudi</button>
+                          <button type="submit" class="btn btn-danger">Elimina</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </form>
             </div>
         </div>
